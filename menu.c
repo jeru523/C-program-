105ff83ee1e77dabb63f3e88d5cfc73b4537d238
jeru523 <atandijerusa@gmail.com>
2022-11-05 00:49:44 +0300

    Adding a menu list

---
+++ menu.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+
+{
+	int beer_brand;
+	int number;
+	int cost;
+	printf("******JAMAL AND DAUHTERS PUB******\n");
+	printf("tusker-1\npilsner-2\nsmirnoff ice-3\nwhite cap-4\n");
+	printf("Enter your choice=");
+	scanf("%d", &beer_brand);
+	if(beer_brand==1)
+	{
+
+		printf("how many bottles of tusker do you need?=");
+		scanf("%d,&number);
+		printf("%d bottles of tusker cost you ksh%d, number,number*100);
+
+	}
+	else if(beer_brand==2)
+
+	{
+		printf("how many bottles of pilsner do you need?=");
+		scanf("%d",&number);
+		printf("%d bottles of pilsner cost you ksh%d\n",number,number*120);
+	}
+	else if(beer_brand==3)
+	{
+		printf("how many bottles of smirnoff ice do you need?=");
+		scanf("%d",&number);
+		printf("%d bottles of smirnof cost you ksh%d\n",number,number*140);
+
+	}
+	else i(beer_brand==4)
+	{
+		printf("how many bottles of white cap do you nedd?=");
+		scanf("%d",&number);
+		printf(%d bottles of white cap cost you ksh%d\n,number,number*90);
+		{
+	else{
+		printf("invalid choice");}
+
+	return 0;
+}
+		
